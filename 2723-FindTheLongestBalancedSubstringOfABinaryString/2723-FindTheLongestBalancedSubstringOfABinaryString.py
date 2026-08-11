36004c97c4500e9a5359c6ec792a8737339dcacb
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:35:03 +0530

    Create 2723-FindTheLongestBalancedSubstringOfABinaryString

---
+++ 2723-FindTheLongestBalancedSubstringOfABinaryString/2723-FindTheLongestBalancedSubstringOfABinaryString.py
@@ -0,0 +1,15 @@
+# Last updated: 8/11/2026, 6:35:02 PM
+class Solution:
+    def findTheLongestBalancedSubstring(self, s: str) -> int:
+        res = 0
+        it = 0
+        while it < len(s):
+            oCnt ,zCnt = 0 , 0
+            while it < len(s) and s[it] == "0"  :
+                zCnt += 1
+                it += 1
+            while it < len(s) and s[it] == "1":
+                oCnt += 1
+                it += 1
+            res = max(res,2*min(oCnt,zCnt))
+        return res
