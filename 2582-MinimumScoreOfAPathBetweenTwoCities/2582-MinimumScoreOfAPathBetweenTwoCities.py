deb0e8fed6c41e3e7a7c0b62b6ad33cd6bd194d4
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:35:12 +0530

    Create 2582-MinimumScoreOfAPathBetweenTwoCities

---
+++ 2582-MinimumScoreOfAPathBetweenTwoCities/2582-MinimumScoreOfAPathBetweenTwoCities.py
@@ -0,0 +1,28 @@
+# Last updated: 8/11/2026, 6:35:11 PM
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = [[] for _ in range(n + 1)]
+
+        for u, v, w in roads:
+            adj[u].append((v, w))
+            adj[v].append((u, w))
+
+        vis = [False] * (n + 1)
+        q = deque([1])
+        vis[1] = True
+
+        ans = float('inf')
+
+        while q:
+
+            node = q.popleft()
+
+            for nei, wt in adj[node]:
+
+                ans = min(ans, wt)
+
+                if not vis[nei]:
+                    vis[nei] = True
+                    q.append(nei)
+
+        return ans
