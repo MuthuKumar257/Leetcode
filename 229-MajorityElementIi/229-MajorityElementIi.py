4fe1a5d108dc97885cdbed33f101aebdcd1b90a4
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:46:20 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.py
@@ -0,0 +1,16 @@
+# Last updated: 8/11/2026, 6:46:19 PM
+class Solution:
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        # Create a Counter to store the count of each element
+        element_count = Counter(nums)
+        
+        majority_elements = []
+        threshold = len(nums) // 3
+        
+        # Iterate through the element count to identify majority elements
+        for element, count in element_count.items():
+            # Check if the element count is greater than the threshold
+            if count > threshold:
+                majority_elements.append(element)
+        
+        return majority_elements
