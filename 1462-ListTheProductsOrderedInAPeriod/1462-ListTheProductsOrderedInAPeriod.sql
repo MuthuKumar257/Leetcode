d070ea7a2181e23661d776224fe5692f5a79954c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:38:16 +0530

    Create 1462-ListTheProductsOrderedInAPeriod

---
+++ 1462-ListTheProductsOrderedInAPeriod/1462-ListTheProductsOrderedInAPeriod.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/11/2026, 6:38:15 PM
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p 
+JOIN Orders o
+ON o.product_id=p.product_id
+WHERE o.order_date like '2020-02-%'
+GROUP BY o.product_id
+HAVING unit>=100; 
