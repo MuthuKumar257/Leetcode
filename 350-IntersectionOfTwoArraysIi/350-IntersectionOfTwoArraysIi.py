a476c7703caf1b29313d126c86bc33b6d83b0302
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:44:56 +0530

    Create 350-IntersectionOfTwoArraysIi

---
+++ 350-IntersectionOfTwoArraysIi/350-IntersectionOfTwoArraysIi.py
@@ -0,0 +1,12 @@
+# Last updated: 8/11/2026, 6:44:55 PM
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2): return self.intersect(nums2, nums1)
+            
+        cnt = Counter(nums1)
+        ans = []
+        for x in nums2:
+            if cnt[x] > 0:
+                ans.append(x)
+                cnt[x] -= 1
+        return ans
