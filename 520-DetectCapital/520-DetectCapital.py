4062776aef47b985f2c45352e0bfb3947bcb8d72
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:43:45 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.py
@@ -0,0 +1,4 @@
+# Last updated: 8/11/2026, 6:43:45 PM
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return word.islower() or word.isupper() or (word[0].isupper() and word[1:].islower())
