5bf9253e3cc9057feb2f3ff0ae73e52051b93f3f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:35:30 +0530

    Create 2392-SuccessfulPairsOfSpellsAndPotions

---
+++ 2392-SuccessfulPairsOfSpellsAndPotions/2392-SuccessfulPairsOfSpellsAndPotions.py
@@ -0,0 +1,14 @@
+# Last updated: 8/11/2026, 6:35:29 PM
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        potion_count = len(potions)
+        result = []
+        for spell_power in spells:
+            required_strength = math.ceil(success / spell_power)
+            if required_strength > potions[-1]:
+                result.append(0)
+                continue
+            index = bisect.bisect_left(potions, required_strength)
+            result.append(potion_count - index)
+        return result
