78c598c0a6d6a95aed28e2d6f419e342f6fecd63
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:47:58 +0530

    Create 166-FractionToRecurringDecimal

---
+++ 166-FractionToRecurringDecimal/166-FractionToRecurringDecimal.py
@@ -0,0 +1,31 @@
+# Last updated: 8/11/2026, 6:47:57 PM
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator == 0:
+            return "0"
+
+        res = []
+        # Handle sign
+        if (numerator < 0) ^ (denominator < 0):
+            res.append("-")
+
+        num, den = abs(numerator), abs(denominator)
+        res.append(str(num // den))
+        remainder = num % den
+        if remainder == 0:
+            return "".join(res)
+
+        res.append(".")
+        seen = {}
+        while remainder:
+            if remainder in seen:
+                idx = seen[remainder]
+                res.insert(idx, "(")
+                res.append(")")
+                return "".join(res)
+            seen[remainder] = len(res)
+            remainder *= 10
+            res.append(str(remainder // den))
+            remainder %= den
+
+        return "".join(res)
