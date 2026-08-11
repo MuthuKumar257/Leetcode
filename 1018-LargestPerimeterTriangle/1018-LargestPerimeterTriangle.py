1e6fed0a4526af7bdf40609f6a314413f8427471
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:40:52 +0530

    Create 1018-LargestPerimeterTriangle

---
+++ 1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.py
@@ -0,0 +1,9 @@
+# Last updated: 8/11/2026, 6:40:51 PM
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
+        
+        while len(nums) > 2 and nums[0] >= nums[1] + nums[2]:
+            nums.pop(0)
+            
+        return 0 if len(nums) < 3 else sum(nums[:3])
