0b1ff1b5e195a4acb2fa9ab7f6f6fd37d9514a66
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:50:47 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.py
@@ -0,0 +1,26 @@
+# Last updated: 8/11/2026, 6:50:46 PM
+class Solution:
+    def RLE(self, sequence):
+        curr = sequence[0]
+        count = 0
+        ans = ""
+        
+        
+        for num in sequence:
+            if curr == num:
+                count += 1
+            else:
+                ans += f'{count}{curr}'
+                curr = num
+                count = 1
+        
+        
+        return ans + f'{count}{curr}'
+    
+    
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        
+        return self.RLE(self.countAndSay(n - 1))
