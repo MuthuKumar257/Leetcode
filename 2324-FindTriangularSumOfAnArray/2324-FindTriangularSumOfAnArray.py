446331fc4261d779e0ebf7dc1d7bb05a5a6640ec
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:35:35 +0530

    Create 2324-FindTriangularSumOfAnArray

---
+++ 2324-FindTriangularSumOfAnArray/2324-FindTriangularSumOfAnArray.py
@@ -0,0 +1,12 @@
+# Last updated: 8/11/2026, 6:35:34 PM
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        l1 = []
+
+        while len(nums) != 1:
+            for i in range(len(nums)-1):
+                number = (nums[i] + nums[i+1]) % 10
+                l1.append(number)
+            nums = l1[:]
+            l1 = []
+        return nums[0]
