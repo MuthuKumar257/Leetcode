ea8e7a351719f23bb9af19cc6d7a8ee2627338e7
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:32:59 +0530

    Create 3626-SmallestDivisibleDigitProductI

---
+++ 3626-SmallestDivisibleDigitProductI/3626-SmallestDivisibleDigitProductI.py
@@ -0,0 +1,18 @@
+# Last updated: 8/11/2026, 6:32:58 PM
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+
+        def getDigitMultiply(num):
+            multiplier = 1
+
+            while multiplier > 0 and num > 0:
+                multiplier *= num % 10
+                num //= 10
+
+            return multiplier
+
+        for num in range(n, 101):
+            if getDigitMultiply(num) % t == 0:
+                return num
+
+        return n
