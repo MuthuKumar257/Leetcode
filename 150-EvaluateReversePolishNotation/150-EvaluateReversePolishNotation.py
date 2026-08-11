b8c5d3fbc40ebc9d4fca00c3d15dca3e3ec61a7c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:48:21 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -0,0 +1,20 @@
+# Last updated: 8/11/2026, 6:48:20 PM
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+
+        for c in tokens:
+            if c == "+":
+                st.append(st.pop() + st.pop())
+            elif c == "-":
+                second, first = st.pop(), st.pop()
+                st.append(first - second)
+            elif c == "*":
+                st.append(st.pop() * st.pop())
+            elif c == "/":
+                second, first = st.pop(), st.pop()
+                st.append(int(first / second))                
+            else:
+                st.append(int(c))
+        
+        return st[0]
