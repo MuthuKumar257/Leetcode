a86f90a6ce5f9979368d02341beb44b91963871c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:34:45 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,30 @@
+// Last updated: 8/11/2026, 6:34:44 PM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let temp = init;
+
+    function increment() {
+        return ++temp;
+    }
+
+    function decrement() {
+        return --temp;
+    }
+
+    function reset() {
+        temp = init;
+        return temp;
+    }
+
+    return { increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
