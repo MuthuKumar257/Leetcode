9611acad7f5315b265c85113ee0b6a1789b5de6f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:34:33 +0530

    Create 2847-FindMaximumNumberOfStringPairs

---
+++ 2847-FindMaximumNumberOfStringPairs/2847-FindMaximumNumberOfStringPairs.py
@@ -0,0 +1,11 @@
+# Last updated: 8/11/2026, 6:34:32 PM
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        strings = set()
+        ans = 0
+        for w in words:
+            if w in strings:
+                ans += 1
+            else:
+                strings.add(w[::-1])
+        return ans
