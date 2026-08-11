9c4a596764e83e8cfb5d9802e3d5f7d9303b1157
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:39:44 +0530

    Create 1228-MinimumCostTreeFromLeafValues

---
+++ 1228-MinimumCostTreeFromLeafValues/1228-MinimumCostTreeFromLeafValues.py
@@ -0,0 +1,25 @@
+# Last updated: 8/11/2026, 6:39:43 PM
+class Solution(object):
+    def mctFromLeafValues(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        ans = 0
+        while(len(arr)>1):
+            mx = arr[0]*arr[1]
+            l = 0
+            r = 1
+            for i in range(len(arr)-1):
+                if arr[i]*arr[i+1]<mx:
+                    mx = arr[i]*arr[i+1]
+                    l = i
+                    r = i+1
+            ans+=mx
+            #print(l,r,mx)
+            if arr[l]>arr[r]:
+                del arr[r]
+            else:
+                del arr[l]
+        return ans
+		
