f002f107bc9c98b41c15f2abc30fb5ac4765b94e
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:37:52 +0530

    Create 1542-ConsecutiveCharacters

---
+++ 1542-ConsecutiveCharacters/1542-ConsecutiveCharacters.py
@@ -0,0 +1,16 @@
+# Last updated: 8/11/2026, 6:37:51 PM
+class Solution(object):
+    def maxPower(self, s):
+        maxcnt=0
+        cnt=0
+
+        for i in range(1,len(s)):
+            
+            if s[i]==s[i-1]:
+                cnt+=1
+                
+            else:
+                cnt=0
+            maxcnt=max(cnt,maxcnt)
+        return maxcnt+1
+        
