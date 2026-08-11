7a9c23db3e403e9847d107ddb2fde3e5836121a1
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:40:31 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.py
@@ -0,0 +1,10 @@
+# Last updated: 8/11/2026, 6:40:30 PM
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        res, opened = [], 0
+        for c in S:
+            if c == '(' and opened > 0: res.append(c)
+            if c == ')' and opened > 1: res.append(c)
+            opened += 1 if c == '(' else -1
+        
+        return "".join(res)
