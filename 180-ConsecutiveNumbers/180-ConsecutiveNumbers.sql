e4a5d2b04f89b0226d30146d13795775e8afa44f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-11 18:47:28 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/11/2026, 6:47:27 PM
+SELECT DISTINCT i1.num AS ConsecutiveNums 
+FROM logs i1, logs i2, logs i3
+WHERE 
+    i1.id = i2.id + 1 
+    AND i2.id = i3.id + 1 
+    AND i1.num = i2.num 
+    AND i2.num = i3.num;
