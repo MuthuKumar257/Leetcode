c86d28bc87f212718dbc255cae8c5b0a36c878bd
Muthu Kumar M <muthukumar82508@gmail.com>
2026-08-27 13:35:43 +0530

    Create 3918-CheckDivisibilityByDigitSumAndProduct

---
+++ 3918-CheckDivisibilityByDigitSumAndProduct/3918-CheckDivisibilityByDigitSumAndProduct.py
@@ -0,0 +1,15 @@
+# Last updated: 8/27/2026, 1:35:42 PM
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        sum_digit = 0
+        product_digit = 1
+        num = n
+
+
+        while num > 0:
+            sum_digit += num % 10
+            product_digit *= num % 10
+            num //= 10
+
+
+        return n % (sum_digit + product_digit) == 0
