f61c812265d0c6b0fb310ca611ea16734a620e6e
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:47:02 +0530

    Create 86-PartitionList

---
+++ 86-PartitionList/86-PartitionList.java
@@ -0,0 +1,27 @@
+// Last updated: 9/11/2026, 9:47:01 AM
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+        
+        while(head != null){
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            }
+            else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+            head = head.next;
+        }
+        
+        leftTail.next = right.next;
+        rightTail.next = null;
+        
+        return left.next;
+    }
+}
