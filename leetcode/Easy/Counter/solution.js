18f379a8affa5c22a358a3ce2c52f763f12c032f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:58:12 +0530

    Add Counter solution (JavaScript)

---
+++ leetcode/Easy/Counter/solution.js
@@ -0,0 +1,24 @@
+/*
+ * LeetCode: Counter
+ * Difficulty: Easy
+ * Language: JavaScript
+ * Problem: https://leetcode.com/problems/counter/
+ */
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
