e1cd525750a9bf1c222455769c7c9b86ce5e97f0
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:40:01 +0530

    Create 978-LongestTurbulentSubarray

---
+++ 978-LongestTurbulentSubarray/978-LongestTurbulentSubarray.py
@@ -0,0 +1,26 @@
+# Last updated: 9/11/2026, 9:39:59 AM
+class Solution:
+    def maxTurbulenceSize(self, A):
+        N = len(A)
+        if N < 2:
+            return N
+
+        ans = 1
+        anchor = 0
+
+        def compare(x, y):
+            if x < y:
+                return -1
+            if x > y:
+                return 1
+            return 0
+
+        for i in range(1, N):
+            c = compare(A[i - 1], A[i])
+            if c == 0:
+                anchor = i
+            else:
+                if i == N - 1 or c * compare(A[i], A[i + 1]) != -1:
+                    ans = max(ans, i - anchor + 1)
+                    anchor = i
+        return ans    
