e9a76ae6e2c5b5b2292e59c5d468433833e56def
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:01:30 +0530

    Add Find the Number of Subsequences With Equal GCD solution (Python)

---
+++ leetcode/Hard/Find-the-Number-of-Subsequences-With-Equal-GCD/solution.py
@@ -0,0 +1,23 @@
+"""
+LeetCode: Find the Number of Subsequences With Equal GCD
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/find-the-number-of-subsequences-with-equal-gcd/
+"""
+
+MOD = 10 ** 9 + 7
+class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
+
+        @functools.cache
+        def dp(idx: int, gcd1: int, gcd2: int) -> int:
+            if idx == len(nums):
+                return int(gcd1 == gcd2 and gcd1 != 0)
+
+            return (
+                dp(idx + 1, gcd1, gcd2) + 
+                dp(idx + 1, gcd(gcd1, nums[idx]), gcd2) + 
+                dp(idx + 1, gcd1, gcd(gcd2, nums[idx]))
+            ) % MOD
+
+        return dp(0, 0, 0)
