b36cdfe20651a2a5026003478278a000b19bb36a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:45:26 +0530

    Add Asteroid Collision solution (Java)

---
+++ leetcode/Medium/Asteroid-Collision/solution.java
@@ -0,0 +1,39 @@
+/*
+ * LeetCode: Asteroid Collision
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/asteroid-collision/
+ */
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0)  {
+                    stack.push(a);
+                }
+
+                if (stack.peek() == -a) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+
+        while(!stack.isEmpty()) {
+            res[i--] = stack.pop();
+        }
+
+        return res;   
+    }
+}
