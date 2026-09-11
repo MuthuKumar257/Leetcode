523bb6474e6ff40e93af61f9d7ae0301571d50fd
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:27:32 +0530

    Add Count and Say solution (Python)

---
+++ leetcode/Medium/Count-and-Say/solution.py
@@ -0,0 +1,32 @@
+"""
+LeetCode: Count and Say
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/count-and-say/
+"""
+
+class Solution:
+    def RLE(self, sequence):
+        curr = sequence[0]
+        count = 0
+        ans = ""
+        
+        
+        for num in sequence:
+            if curr == num:
+                count += 1
+            else:
+                ans += f'{count}{curr}'
+                curr = num
+                count = 1
+        
+        
+        return ans + f'{count}{curr}'
+    
+    
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        
+        return self.RLE(self.countAndSay(n - 1))
