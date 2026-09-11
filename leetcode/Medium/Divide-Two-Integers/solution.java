00c4f51b9d810cae0503aaf5ed0378d9971fc280
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:26:53 +0530

    Add Divide Two Integers solution (Java)

---
+++ leetcode/Medium/Divide-Two-Integers/solution.java
@@ -0,0 +1,15 @@
+/*
+ * LeetCode: Divide Two Integers
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/divide-two-integers/
+ */
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+       if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+       return dividend/divisor; 
+    }
+}
