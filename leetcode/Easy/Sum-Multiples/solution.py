83251c7f4a81d9288902e897923ecbc555abdfcd
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:58:22 +0530

    Add Sum Multiples solution (Python)

---
+++ leetcode/Easy/Sum-Multiples/solution.py
@@ -0,0 +1,14 @@
+"""
+LeetCode: Sum Multiples
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/sum-multiples/
+"""
+
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        s=0
+        for i in range(1,n+1):
+            if i%3==0 or i%5==0 or i%7==0:
+                s+=i
+        return s
