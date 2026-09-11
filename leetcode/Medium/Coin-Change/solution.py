199da7dcf73122688a56bf6bbac18fa21e504367
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:38:27 +0530

    Add Coin Change solution (Python)

---
+++ leetcode/Medium/Coin-Change/solution.py
@@ -0,0 +1,25 @@
+"""
+LeetCode: Coin Change
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/coin-change/
+"""
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        self.memo = {}
+        return self._solve(coins, amount)  
+    def _solve(self, coins, rem):
+        if rem < 0:
+            return -1
+        if rem == 0:
+            return 0
+        if rem in self.memo:
+            return self.memo[rem]
+        min_count = float('inf')
+        for coin in coins:
+            res = self._solve(coins, rem - coin)
+            if res != -1:
+                min_count = min(min_count, 1 + res)
+        self.memo[rem] = min_count if min_count != float('inf') else -1
+        return self.memo[rem]
