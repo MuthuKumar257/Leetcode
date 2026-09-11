5efde0178adc0b336547ed40523c723446d21560
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:06:13 +0530

    Add Maximum Total Value solution (Python)

---
+++ leetcode/Hard/Maximum-Total-Value/solution.py
@@ -0,0 +1,36 @@
+"""
+LeetCode: Maximum Total Value
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/maximum-total-value/
+"""
+
+class Solution:
+    def maxTotalValue(self, value: list[int], decay: list[int], m: int) -> int:
+        MOD=10**9+7
+        def count(x):
+            c=0
+            for v,d in zip(value,decay):
+                if v<x:
+                    continue
+                c+=(v-x)//d+1
+            return c
+        l,r=1,max(value)
+        while l<=r:
+            mid=(l+r)//2
+            if count(mid)>=m:
+                l=mid+1
+            else:
+                r=mid-1
+        T=r
+        res=0
+        cmp=0
+        for i,j in zip(value,decay):
+            if i<T+1:
+                continue
+            k=(i-(T+1))//j+1
+            cmp+=k
+            last=i-(k-1)*j
+            res+=k*(i+last)//2
+        res+=(m-cmp)*T
+        return res%MOD
