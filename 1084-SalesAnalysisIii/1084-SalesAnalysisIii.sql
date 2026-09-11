bbf7cf0aee2eae2501f29229957d514da7bec7be
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:39:03 +0530

    Create 1084-SalesAnalysisIii

---
+++ 1084-SalesAnalysisIii/1084-SalesAnalysisIii.sql
@@ -0,0 +1,11 @@
+-- Last updated: 9/11/2026, 9:39:01 AM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+SELECT p.product_id ,p.product_name
+FROM product p
+JOIN sales s
+    ON p.product_id=s.product_id
+GROUP BY p.product_id,p.product_name
+HAVING MIN(s.sale_date)>= '2019-01-01'
+    AND MAX(s.sale_date)<= '2019-03-31'
