d3bd5bf7c99e659cf2e31fbe4120677c3ee089c6
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:57:07 +0530

    Add Add Two Integers solution (Python)

---
+++ leetcode/Easy/Add-Two-Integers/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Add Two Integers
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/add-two-integers/
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
