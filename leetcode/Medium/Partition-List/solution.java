3a766475842145adeb70d3f52f9a17d60ca60e61
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:30:07 +0530

    Add Partition List solution (Java)

---
+++ leetcode/Medium/Partition-List/solution.java
@@ -0,0 +1,33 @@
+/*
+ * LeetCode: Partition List
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/partition-list/
+ */
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+        
+        while(head != null){
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            }
+            else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+            head = head.next;
+        }
+        
+        leftTail.next = right.next;
+        rightTail.next = null;
+        
+        return left.next;
+    }
+}
