8509b05439d3e5c6c635344764f2860dc39b7da4
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:37:37 +0530

    Create 1260-Shift2dGrid

---
+++ 1260-Shift2dGrid/1260-Shift2dGrid.py
@@ -0,0 +1,6 @@
+# Last updated: 9/11/2026, 9:37:36 AM
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        flat,m,n = list(chain.from_iterable(grid)), len(grid), len(grid[0])
+        return  [(flat[-(k % (m*n)):]+flat)[i*n : (i+1)*n] for i in range(m)]
+        
