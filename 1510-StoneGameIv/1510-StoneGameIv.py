b620bcee2e95e8c0c92701c6fc753bf087ebcb6c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:33:52 +0530

    Create 1510-StoneGameIv

---
+++ 1510-StoneGameIv/1510-StoneGameIv.py
@@ -0,0 +1,10 @@
+# Last updated: 9/11/2026, 9:33:51 AM
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+        for i in range(1, n + 1):
+            for j in range(1, isqrt(i) + 1):
+                if not dp[i - j * j]:
+                    dp[i] = True
+                    break
+        return dp[n]
