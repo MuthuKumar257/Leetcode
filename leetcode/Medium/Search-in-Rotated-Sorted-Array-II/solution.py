ee6fb6290cdb727538236e38062a263a03b48b1f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:29:45 +0530

    Add Search in Rotated Sorted Array II solution (Python)

---
+++ leetcode/Medium/Search-in-Rotated-Sorted-Array-II/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Search in Rotated Sorted Array II
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        return target in nums
