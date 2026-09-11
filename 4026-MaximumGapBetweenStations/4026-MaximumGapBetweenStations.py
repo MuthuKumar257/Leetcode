918d02e571d3fa6c43b07e2a1fab41a616fdcb8d
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:22:47 +0530

    Create 4026-MaximumGapBetweenStations

---
+++ 4026-MaximumGapBetweenStations/4026-MaximumGapBetweenStations.py
@@ -0,0 +1,22 @@
+# Last updated: 9/11/2026, 9:22:45 AM
+class Solution:
+    def maximumGap(self, skill: str, station: str) -> int:
+        n=len(skill)
+        left=[0]*n
+        right=[0]*n
+        j=0
+        for i in range(n):
+            while station[j]!=skill[i]:
+                j+=1
+            left[i]=j
+            j+=1
+        j=len(station)-1
+        for i in range(n-1,-1,-1):
+            while station[j]!=skill[i]:
+                j-=1
+            right[i]=j
+            j-=1
+        ans=0
+        for i in range(1,n):
+            ans=max(ans,right[i]-left[i-1])
+        return ans
