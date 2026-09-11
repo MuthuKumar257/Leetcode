068fbc46772be0e19794617edb066479122ce8be
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:55:48 +0530

    Add Final Value of Variable After Performing Operations solution (Python)

---
+++ leetcode/Easy/Final-Value-of-Variable-After-Performing-Operations/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Final Value of Variable After Performing Operations
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+"""
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        return sum(1 if op[1]=='+' else -1 for op in operations)
