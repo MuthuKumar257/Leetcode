8b96ad200ac8ee740d4479812968027cb865c56d
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:24:22 +0530

    Create 3514-NumberOfUniqueXorTripletsIi

---
+++ 3514-NumberOfUniqueXorTripletsIi/3514-NumberOfUniqueXorTripletsIi.java
@@ -0,0 +1,32 @@
+// Last updated: 9/11/2026, 9:24:20 AM
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        final int MAX_XOR = 2048;
+
+        boolean[] pairXor = new boolean[MAX_XOR];
+        boolean[] tripletXor = new boolean[MAX_XOR];
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pairXor[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        for (int x = 0; x < MAX_XOR; x++) {
+            if (!pairXor[x]) continue;
+
+            for (int v : nums) {
+                tripletXor[x ^ v] = true;
+            }
+        }
+
+        int count = 0;
+        for (boolean exists : tripletXor) {
+            if (exists) count++;
+        }
+
+        return count;
+    }
+}
