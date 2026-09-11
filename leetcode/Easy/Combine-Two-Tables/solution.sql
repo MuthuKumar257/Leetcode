eb09662d4c611adcb5410bc1749ffd964f2ce5f1
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:34:08 +0530

    Add Combine Two Tables solution (MySQL)

---
+++ leetcode/Easy/Combine-Two-Tables/solution.sql
@@ -0,0 +1,12 @@
+-- LeetCode: Combine Two Tables
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/combine-two-tables/
+
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p
+LEFT JOIN Address a ON p.personId = a.personId;
