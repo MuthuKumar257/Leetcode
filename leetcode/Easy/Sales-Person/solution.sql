c1d894218d5af968edf88666fac4ce88c239d380
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:43:11 +0530

    Add Sales Person solution (MySQL)

---
+++ leetcode/Easy/Sales-Person/solution.sql
@@ -0,0 +1,11 @@
+-- LeetCode: Sales Person
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/sales-person/
+
+SELECT s.name FROM SalesPerson s 
+	WHERE s.sales_id NOT IN (
+		SELECT o.sales_id FROM Orders o LEFT JOIN Company c 
+			ON c.com_id=o.com_id 
+		WHERE c.name="RED"
+	);
