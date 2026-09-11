27d03e38ad9e3141bc978d0d18eee4077b62afcd
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:50:16 +0530

    Add Queries Quality and Percentage solution (MySQL)

---
+++ leetcode/Easy/Queries-Quality-and-Percentage/solution.sql
@@ -0,0 +1,13 @@
+-- LeetCode: Queries Quality and Percentage
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/queries-quality-and-percentage/
+
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+SELECT query_name, 
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND((SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END) / count(*)) * 100,2) AS poor_query_percentage
+FROM queries
+GROUP BY query_name
