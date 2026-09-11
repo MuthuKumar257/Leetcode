b7ebc60fcc48fbf3627aa5763694efd6c4647001
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:35:45 +0530

    Add House Robber solution (Python)

---
+++ leetcode/Medium/House-Robber/solution.py
@@ -0,0 +1,15 @@
+"""
+LeetCode: House Robber
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/house-robber/
+"""
+
+class Solution(object):
+    def rob(self, nums):
+        rob, norob = 0, 0
+        for num in nums:
+            newRob = norob + num
+            newNoRob = max(norob, rob)
+            rob, norob = newRob, newNoRob
+        return max(rob, norob)
