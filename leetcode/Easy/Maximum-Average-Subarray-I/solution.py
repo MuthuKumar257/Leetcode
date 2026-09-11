4f74e3810641a982f5df3c4c2b5b143de86d66ca
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:07 +0530

    Add Maximum Average Subarray I solution (Python)

---
+++ leetcode/Easy/Maximum-Average-Subarray-I/solution.py
@@ -0,0 +1,16 @@
+"""
+LeetCode: Maximum Average Subarray I
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/maximum-average-subarray-i/
+"""
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        w_sum = sum(nums[:k])
+        max_sum = w_sum
+
+        for i in range(k,len(nums)):
+            w_sum += nums[i] - nums[i-k]
+            max_sum  = max(max_sum,w_sum)
+        return max_sum/k
