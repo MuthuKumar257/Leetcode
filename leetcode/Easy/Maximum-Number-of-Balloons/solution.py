930292b0060b929c121f996a0b4e6200637bac30
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:50:03 +0530

    Add Maximum Number of Balloons solution (Python)

---
+++ leetcode/Easy/Maximum-Number-of-Balloons/solution.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: Maximum Number of Balloons
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/maximum-number-of-balloons/
+"""
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = defaultdict(int)
+        word = "balloon"
+        for t in text:
+            if t in word:
+                d[t] +=1
+        
+        if any(t not in d for t in word):
+            return 0
+        else:
+            return min(d['b']//1, d['a']//1, d['l']//2, d['o']//2, d['n']//1)
