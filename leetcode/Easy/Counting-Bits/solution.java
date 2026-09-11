c30d9aefd3e53fec0614fd1bbbd9decced071d30
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:38:42 +0530

    Add Counting Bits solution (Java)

---
+++ leetcode/Easy/Counting-Bits/solution.java
@@ -0,0 +1,23 @@
+/*
+ * LeetCode: Counting Bits
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/counting-bits/
+ */
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        int sub = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (sub * 2 == i) {
+                sub = i;
+            }
+
+            dp[i] = dp[i - sub] + 1;
+        }
+
+        return dp;        
+    }
+}
