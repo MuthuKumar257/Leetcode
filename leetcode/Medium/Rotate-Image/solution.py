3ef0f2e8224f088f87c8724e40bdea60eac21b21
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:27:54 +0530

    Add Rotate Image solution (Python)

---
+++ leetcode/Medium/Rotate-Image/solution.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: Rotate Image
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/rotate-image/
+"""
+
+class Solution:
+
+    def rotate(self, matrix: list[list[int]]) -> None:
+        row = len(matrix)
+        col = len(matrix[0])
+
+        for i in range(row):
+            for j in range(i + 1, col):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for k in range(row):
+            matrix[k].reverse()
