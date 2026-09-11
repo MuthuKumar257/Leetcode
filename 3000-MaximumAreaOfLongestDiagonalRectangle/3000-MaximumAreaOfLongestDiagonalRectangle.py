306f34c1ea1f689081d6a3a5061b939497d8fb2e
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:26:10 +0530

    Create 3000-MaximumAreaOfLongestDiagonalRectangle

---
+++ 3000-MaximumAreaOfLongestDiagonalRectangle/3000-MaximumAreaOfLongestDiagonalRectangle.py
@@ -0,0 +1,18 @@
+# Last updated: 9/11/2026, 9:26:08 AM
+import math
+
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: list[list[int]]) -> int:
+        maxi = -1.0
+        res = 0
+        
+        for l, b in dimensions:
+            d = math.sqrt(l * l + b * b)
+            
+            if d > maxi:
+                maxi = d
+                res = l * b
+            elif d == maxi:
+                res = max(res, l * b)
+        
+        return res
