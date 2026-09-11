04086b93b7d96f74555bcea5f675a25fd8033dee
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:40:17 +0530

    Add Longest Repeating Character Replacement solution (Python)

---
+++ leetcode/Medium/Longest-Repeating-Character-Replacement/solution.py
@@ -0,0 +1,22 @@
+"""
+LeetCode: Longest Repeating Character Replacement
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/longest-repeating-character-replacement/
+"""
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        max_count = 0
+        left = 0
+        freq = {}
+        
+        for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
+            max_count = max(max_count, freq[s[right]])
+            
+            if right - left + 1 - max_count > k:
+                freq[s[left]] -= 1
+                left += 1
+                
+        return len(s) - left
