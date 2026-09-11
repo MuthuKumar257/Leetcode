da4ee326d98c214277d3a08c9507a9fd7325bb33
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:48:49 +0530

    Add Actors and Directors Who Cooperated At Least Three Times solution (MySQL)

---
+++ leetcode/Easy/Actors-and-Directors-Who-Cooperated-At-Least-Three-Times/solution.sql
@@ -0,0 +1,11 @@
+-- LeetCode: Actors and Directors Who Cooperated At Least Three Times
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+# Write your MySQL query statement below
+SELECT actor_id,
+director_id
+FROM ActorDirector 
+GROUP BY actor_id,director_id
+HAVING COUNT(*)>=3;
