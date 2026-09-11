4d3b41fd7a35cb1b2c1bb9557bde430985a9bd61
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:39:53 +0530

    Add Split Array Largest Sum solution (Java)

---
+++ leetcode/Hard/Split-Array-Largest-Sum/solution.java
@@ -0,0 +1,40 @@
+/*
+ * LeetCode: Split Array Largest Sum
+ * Difficulty: Hard
+ * Language: Java
+ * Problem: https://leetcode.com/problems/split-array-largest-sum/
+ */
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start = Integer.MIN_VALUE, end = 0;
+        for (int num : nums) {
+            start = Math.max(start, num);
+            end += num;
+        }
+        int ans = 0;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int subarrays = 1, currSum = 0;
+            
+            for (int num : nums) {
+                if (currSum + num > mid) {
+                    subarrays++;
+                    currSum = num;
+                } else {
+                    currSum += num;
+                }
+            }
+            
+            if (subarrays <= k) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
