c82f85696129dcad1dfbb0880c946676ad31b794
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:42:18 +0530

    Add Permutation in String solution (Python)

---
+++ leetcode/Medium/Permutation-in-String/solution.py
@@ -0,0 +1,31 @@
+"""
+LeetCode: Permutation in String
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/permutation-in-string/
+"""
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n2 < n1:
+            return False
+
+        c1 = [0] * 26
+        c2 = [0] * 26
+
+        for i in range(n1):
+            c1[ord(s1[i]) - ord('a')] += 1
+            c2[ord(s2[i]) - ord('a')] += 1
+
+        if c1 == c2:
+            return True
+
+        for i in range(n1, n2):
+            c2[ord(s2[i]) - ord('a')] += 1
+            c2[ord(s2[i - n1]) - ord('a')] -= 1
+
+            if c1 == c2:
+                return True
+
+        return False
