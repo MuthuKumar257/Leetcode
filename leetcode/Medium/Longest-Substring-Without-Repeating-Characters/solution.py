680c114b71d4dcb57b498489f33c427c0a9db896
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:24:38 +0530

    Add Longest Substring Without Repeating Characters solution (Python)

---
+++ leetcode/Medium/Longest-Substring-Without-Repeating-Characters/solution.py
@@ -0,0 +1,37 @@
+"""
+LeetCode: Longest Substring Without Repeating Characters
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         a=[0]
+#         for j in range(len(s)):
+#             temp=s[j]
+#             for i in range(j+1,len(s)):
+#                 if s[i] in temp:
+#                     a.append(len(temp))
+#                     temp=s[i]
+#                     break
+#                 temp=temp+s[i]
+#             a.append(len(temp))
+#         return max(a)
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        left = 0
+        max_length = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
