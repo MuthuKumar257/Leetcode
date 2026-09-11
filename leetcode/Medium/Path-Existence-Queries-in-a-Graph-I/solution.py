7b3e2bc316244e766fa1d1f8b28f5c2861199810
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:02:51 +0530

    Add Path Existence Queries in a Graph I solution (Python)

---
+++ leetcode/Medium/Path-Existence-Queries-in-a-Graph-I/solution.py
@@ -0,0 +1,16 @@
+"""
+LeetCode: Path Existence Queries in a Graph I
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/path-existence-queries-in-a-graph-i/
+"""
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        p = [i for i in range(n)]
+
+        for i in range(1, n):
+            if nums[i]-nums[i-1] <= maxDiff: 
+                p[i] = p[i-1]
+        
+        return [p[i] == p[j] for i,j in queries]
