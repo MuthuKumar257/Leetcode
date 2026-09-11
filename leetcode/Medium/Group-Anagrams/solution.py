51a1d4a195571983e608f6b8b5157fee28aa21a1
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:27:59 +0530

    Add Group Anagrams solution (Python)

---
+++ leetcode/Medium/Group-Anagrams/solution.py
@@ -0,0 +1,16 @@
+"""
+LeetCode: Group Anagrams
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/group-anagrams/
+"""
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
