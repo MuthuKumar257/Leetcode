3a5aab42a6b492b22f7a35687db8e8fc255150d4
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:48:13 +0530

    Add Capacity To Ship Packages Within D Days solution (Java)

---
+++ leetcode/Medium/Capacity-To-Ship-Packages-Within-D-Days/solution.java
@@ -0,0 +1,31 @@
+/*
+ * LeetCode: Capacity To Ship Packages Within D Days
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ */
+
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int high = 0 , low = 0 ;
+        for( int i : weights ) {
+            high += i ;
+            low = Math.max( low , i ) ;
+        }
+        return find( low , high , weights , days , 0 );
+    }
+    private int find(int start , int end , int  w[] , int  days , int res ) {
+        int d = days;
+        if( start > end )   return  res ;
+        int mid = start + ( end - start ) / 2 , i = 0 ;
+        while( i < w.length ) {
+            int sum = 0 ;
+            while( i < w.length && sum + w[i] <= mid ){
+                sum += w [ i++ ];
+            }
+            d -- ;
+            if( d < 0 )    return find( mid + 1 , end , w , days ,res ) ;
+        }if( d >= 0 )    return find( start , mid - 1 , w , days ,mid ) ;
+        else    return   find( mid + 1 , end , w , days , res ) ;
+    }
+}
