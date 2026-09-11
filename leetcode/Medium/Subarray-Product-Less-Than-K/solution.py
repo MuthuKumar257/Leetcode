444709d65b519a9c780a65ccb51f923f068bdc3c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:45:01 +0530

    Add Subarray Product Less Than K solution (Python)

---
+++ leetcode/Medium/Subarray-Product-Less-Than-K/solution.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: Subarray Product Less Than K
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/subarray-product-less-than-k/
+"""
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:        
+        left, prod, count = 0, 1, 0
+                
+        for right in range(len(nums)):
+            prod *= nums[right]            
+            
+            while prod >= k and left <= right:                    
+                prod /= nums[left]
+                left += 1                        
+            count += right - left + 1                
+        return count
