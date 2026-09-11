14d148b5cd81015b764c8ffe34ef94135316b598
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:32:50 +0530

    Create 1704-DetermineIfStringHalvesAreAlike

---
+++ 1704-DetermineIfStringHalvesAreAlike/1704-DetermineIfStringHalvesAreAlike.py
@@ -0,0 +1,10 @@
+# Last updated: 9/11/2026, 9:32:48 AM
+class Solution:
+
+    def halvesAreAlike(self, S: str) -> bool:
+        vowels = "aeiouAEIOU"
+        mid, ans = len(S) // 2, 0
+        for i in range(mid):
+            if S[i] in vowels: ans += 1
+            if S[mid+i] in vowels: ans -=1
+        return ans == 0
