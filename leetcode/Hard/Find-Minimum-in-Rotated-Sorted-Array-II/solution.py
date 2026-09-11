f27adc63ace90ec48f3153c51d7afa6b019f29eb
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:33:13 +0530

    Add Find Minimum in Rotated Sorted Array II solution (Python)

---
+++ leetcode/Hard/Find-Minimum-in-Rotated-Sorted-Array-II/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Find Minimum in Rotated Sorted Array II
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+"""
+
+class Solution(object):
+    def findMin(self, nums):
+        return min(nums)
