391423d41b0ff69516190de63f9cd4a2f128a79a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:05:26 +0530

    Add Count Commas in Range solution (Python)

---
+++ leetcode/Easy/Count-Commas-in-Range/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Count Commas in Range
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/count-commas-in-range/
+"""
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        return max(0,n-999)
