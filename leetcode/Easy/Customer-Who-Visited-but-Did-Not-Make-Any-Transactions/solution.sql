7c1082b3b1b39c80f88cfcdefc866669623f006c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:53:22 +0530

    Add Customer Who Visited but Did Not Make Any Transactions solution (MySQL)

---
+++ leetcode/Easy/Customer-Who-Visited-but-Did-Not-Make-Any-Transactions/solution.sql
@@ -0,0 +1,11 @@
+-- LeetCode: Customer Who Visited but Did Not Make Any Transactions
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+# Write your MySQL query statement below
+
+select customer_id , count(visit_id) as count_no_trans from Visits
+where 
+visit_id not in (select  visit_id from Transactions)
+group by customer_id;
