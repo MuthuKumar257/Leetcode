e39b85a294ebfe0a8e9874e6c6673fe0c12e9dde
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:30:25 +0530

    Create 2130-MaximumTwinSumOfALinkedList

---
+++ 2130-MaximumTwinSumOfALinkedList/2130-MaximumTwinSumOfALinkedList.py
@@ -0,0 +1,27 @@
+# Last updated: 9/11/2026, 9:30:24 AM
+from collections import deque
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        container = deque()
+
+        iterNode = head
+
+        while iterNode is not None:
+            container.append(iterNode.val)
+            iterNode = iterNode.next
+
+        result = 0
+
+        while container:
+            front = container.popleft()
+            back = container.pop()
+            result = max(result, front + back)
+
+        return result
