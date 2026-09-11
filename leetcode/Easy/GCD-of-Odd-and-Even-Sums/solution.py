28c6ee9a87f31e3820914513c7f879707f3414db
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:03:49 +0530

    Add GCD of Odd and Even Sums solution (Python)

---
+++ leetcode/Easy/GCD-of-Odd-and-Even-Sums/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: GCD of Odd and Even Sums
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+"""
+
+class Solution:
+    def gcdOfOddEvenSums(self,n:int)->int:
+        return n
