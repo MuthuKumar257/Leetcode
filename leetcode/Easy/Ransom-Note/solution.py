68ba09e4e0c1cd2f94223f44d8df56e7ebd33e5f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:39:28 +0530

    Add Ransom Note solution (Python)

---
+++ leetcode/Easy/Ransom-Note/solution.py
@@ -0,0 +1,20 @@
+"""
+LeetCode: Ransom Note
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/ransom-note/
+"""
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        
+        chars = {}
+        for char in magazine:
+            if char not in chars: chars[char] = 1
+            else: chars[char] += 1
+
+        for char in ransomNote:
+            if char in chars and chars[char] > 0: chars[char] -= 1
+            else: return False
+        
+        return True
