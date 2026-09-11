021a083d157ee4051b959417672d56eaa2f5ff62
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:58:17 +0530

    Add Sleep solution (JavaScript)

---
+++ leetcode/Easy/Sleep/solution.js
@@ -0,0 +1,19 @@
+/*
+ * LeetCode: Sleep
+ * Difficulty: Easy
+ * Language: JavaScript
+ * Problem: https://leetcode.com/problems/sleep/
+ */
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve,millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
