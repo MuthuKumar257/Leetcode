c8e0b4a5e2fb4b21339e24b6cf3421b702dd7d73
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:29:13 +0530

    Add Climbing Stairs solution (Java)

---
+++ leetcode/Easy/Climbing-Stairs/solution.java
@@ -0,0 +1,19 @@
+/*
+ * LeetCode: Climbing Stairs
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/climbing-stairs/
+ */
+
+class Solution {
+    static int[] dp=new int[46];
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        return dp[n]=climbStairs(n-1) + climbStairs(n-2);
+    }
+}
