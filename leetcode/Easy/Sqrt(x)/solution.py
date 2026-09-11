75d3fb92aa6c6f91e3abdb48c939dbb9fb982b71
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:29:08 +0530

    Add Sqrt(x) solution (Python)

---
+++ leetcode/Easy/Sqrt(x)/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Sqrt(x)
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/sqrtx/
+"""
+
+class Solution(object):
+    def mySqrt(self, x):
+        return int(floor(x**0.5))
