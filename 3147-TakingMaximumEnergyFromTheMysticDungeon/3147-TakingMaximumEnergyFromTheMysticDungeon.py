ba0181785c84a3a84bdf6ed3673c73dc41531f1a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:25:31 +0530

    Create 3147-TakingMaximumEnergyFromTheMysticDungeon

---
+++ 3147-TakingMaximumEnergyFromTheMysticDungeon/3147-TakingMaximumEnergyFromTheMysticDungeon.py
@@ -0,0 +1,7 @@
+# Last updated: 9/11/2026, 9:25:29 AM
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        last_k = energy[-k:][::-1]
+        for idx in range(len(energy) - k - 1, -1, -1):
+            last_k.append(last_k[-k] + energy[idx])
+        return max(last_k)
