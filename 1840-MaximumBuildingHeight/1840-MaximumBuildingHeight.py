d03a3328819b95488f30d3f36ea83959ddca3f54
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:32:04 +0530

    Create 1840-MaximumBuildingHeight

---
+++ 1840-MaximumBuildingHeight/1840-MaximumBuildingHeight.py
@@ -0,0 +1,9 @@
+# Last updated: 9/11/2026, 9:32:00 AM
+class Solution:
+    def maxBuilding(self, n: int, re: List[List[int]]) -> int:
+        re.sort(key=lambda i:i[0]+i[1])
+        ans, b = 0, [1,0]
+        for r in re:
+            ans = max(ans,(min(2*n-b[0]+b[1],r[0]+r[1])-(b[0]-b[1]))//2)
+            if b[0]-b[1] < r[0]-r[1]: b = [r[0],r[1]]
+        return max(ans,n-b[0]+b[1])
