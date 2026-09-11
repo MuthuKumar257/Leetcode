e918ed39ecb958a0d124f7be9f210e104762cc2f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:32:25 +0530

    Add Maximum Product of Two Elements in an Array solution (Java)

---
+++ leetcode/Easy/Maximum-Product-of-Two-Elements-in-an-Array/solution.java
@@ -0,0 +1,25 @@
+/*
+ * LeetCode: Maximum Product of Two Elements in an Array
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ */
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for(int i : nums){
+            if(i > max1){
+                max2 = max1;
+                max1 = i;
+            }
+            else if(i > max2){
+                max2 = i;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
