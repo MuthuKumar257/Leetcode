4b6ea7b4018dd89d0391dd04362e293473c50661
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:25 +0530

    Add Valid Parenthesis String solution (Java)

---
+++ leetcode/Medium/Valid-Parenthesis-String/solution.java
@@ -0,0 +1,29 @@
+/*
+ * LeetCode: Valid Parenthesis String
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/valid-parenthesis-string/
+ */
+
+public class Solution {
+    public boolean checkValidString(String s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+}
