561a2dc49310534c8a15821917d6454d3df1edc8
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:16 +0530

    Add Palindromic Substrings solution (Python)

---
+++ leetcode/Medium/Palindromic-Substrings/solution.py
@@ -0,0 +1,29 @@
+"""
+LeetCode: Palindromic Substrings
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/palindromic-substrings/
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        total = n
+
+        center = 0.0
+        while center < n:
+            if center.is_integer():
+                left, right = int(center) - 1, int(center) + 1
+            else:
+                left, right = floor(center), ceil(center)
+            
+            while left >= 0 and right < n:
+                if s[left] != s[right]:
+                    break
+                total += 1
+                left -= 1
+                right += 1
+            
+            center += 0.5
+        
+        return total
