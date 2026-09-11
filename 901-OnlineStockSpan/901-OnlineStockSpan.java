86f9ff296fe9009ba79f20741d848c3c49c3b612
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:41:13 +0530

    Create 901-OnlineStockSpan

---
+++ 901-OnlineStockSpan/901-OnlineStockSpan.java
@@ -0,0 +1,18 @@
+// Last updated: 9/11/2026, 9:41:11 AM
+class StockSpanner {
+
+    Stack<int[]> stack = new Stack<>();
+    public int next(int price) {
+        int res = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price)
+            res += stack.pop()[1];
+        stack.push(new int[]{price, res});
+        return res;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
