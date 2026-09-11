8634ef438a7798717b504514017169aa38bc914c
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:41:19 +0530

    Create 876-MiddleOfTheLinkedList

---
+++ 876-MiddleOfTheLinkedList/876-MiddleOfTheLinkedList.py
@@ -0,0 +1,11 @@
+# Last updated: 9/11/2026, 9:41:17 AM
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_pointer = head
+        fast_pointer = head
+
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
