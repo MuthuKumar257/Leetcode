666a84390e198f619eaa8709d9674b30c4a12cd8
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:03:34 +0530

    Add Process String with Special Operations II solution (Python)

---
+++ leetcode/Hard/Process-String-with-Special-Operations-II/solution.py
@@ -0,0 +1,45 @@
+"""
+LeetCode: Process String with Special Operations II
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/process-string-with-special-operations-ii/
+"""
+
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        length = 0
+
+        # Step 1: compute final length
+        for c in s:
+            if c == '*':
+                length = max(0, length - 1)
+            elif c == '#':
+                length *= 2
+            elif c != '%':
+                length += 1
+
+        if k >= length:
+            return '.'
+
+        # Step 2: backward simulation
+        for i in range(len(s) - 1, -1, -1):
+            c = s[i]
+
+            if c == '*':
+                length += 1
+
+            elif c == '#':
+                half = length // 2
+                if k >= half:
+                    k -= half
+                length = half
+
+            elif c == '%':
+                k = length - 1 - k
+
+            else:
+                if k == length - 1:
+                    return c
+                length -= 1
+
+        return '.'
