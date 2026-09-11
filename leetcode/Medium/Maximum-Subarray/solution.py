22f2aee1b08502674ca4c1c891a0a73684b58faf
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:28:13 +0530

    Add Maximum Subarray solution (Python)

---
+++ leetcode/Medium/Maximum-Subarray/solution.py
@@ -0,0 +1,16 @@
+"""
+LeetCode: Maximum Subarray
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/maximum-subarray/
+"""
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:        
+        newNum = maxTotal = nums[0]        
+        
+        for i in range(1, len(nums)):
+            newNum = max(nums[i], nums[i] + newNum)
+            maxTotal = max(newNum, maxTotal)
+
+        return maxTotal
