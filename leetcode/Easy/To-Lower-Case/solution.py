e04304fbff29c7e625866d5723b1dfb8c02508b9
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:56 +0530

    Add To Lower Case solution (Python)

---
+++ leetcode/Easy/To-Lower-Case/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: To Lower Case
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/to-lower-case/
+"""
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+       return s.lower()
