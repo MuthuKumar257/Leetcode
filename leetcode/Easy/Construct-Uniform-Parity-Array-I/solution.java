8ee92cadd16f73fc6c824fed158e74ad02cff213
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:05:37 +0530

    Add Construct Uniform Parity Array I solution (Java)

---
+++ leetcode/Easy/Construct-Uniform-Parity-Array-I/solution.java
@@ -0,0 +1,12 @@
+/*
+ * LeetCode: Construct Uniform Parity Array I
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
