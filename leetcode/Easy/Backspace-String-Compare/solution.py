30c2f38c0a6ed89bb70b1e235e9dbc343c89875e
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:46:00 +0530

    Add Backspace String Compare solution (Python)

---
+++ leetcode/Easy/Backspace-String-Compare/solution.py
@@ -0,0 +1,20 @@
+"""
+LeetCode: Backspace String Compare
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/backspace-string-compare/
+"""
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+
+        def remove_characters(s):
+            stack = []
+            for char in s:
+                if char == '#' and stack:
+                    stack.pop()
+                elif char != '#':
+                    stack.append(char)
+            return stack
+
+        return remove_characters(s) == remove_characters(t)
