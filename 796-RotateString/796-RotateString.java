9e065c82a2e4bc2b44c313c682d3a89c7c868d13
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:41:34 +0530

    Create 796-RotateString

---
+++ 796-RotateString/796-RotateString.java
@@ -0,0 +1,30 @@
+// Last updated: 9/11/2026, 9:41:32 AM
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if(A == null || B == null) {
+            //throw exception on A and B both being null?
+            return false;
+        }
+        if(A.length() != B.length()) {
+            return false;
+        }
+        if(A.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < A.length(); i++) {
+            if(rotateString(A, B, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean rotateString(String A, String B, int rotation) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A.charAt(i) != B.charAt((i+rotation)%B.length())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
