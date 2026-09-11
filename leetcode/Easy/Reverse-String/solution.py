386428ae7f55daf8a0e49f33d0475800b4e17286
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:38:51 +0530

    Add Reverse String solution (Python)

---
+++ leetcode/Easy/Reverse-String/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Reverse String
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/reverse-string/
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        return s.reverse()
