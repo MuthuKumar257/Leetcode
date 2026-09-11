8fcd673e6791aa5a096ebf7e68d51a55cb13bfbd
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:38:01 +0530

    Create 1189-MaximumNumberOfBalloons

---
+++ 1189-MaximumNumberOfBalloons/1189-MaximumNumberOfBalloons.py
@@ -0,0 +1,13 @@
+# Last updated: 9/11/2026, 9:38:00 AM
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = defaultdict(int)
+        word = "balloon"
+        for t in text:
+            if t in word:
+                d[t] +=1
+        
+        if any(t not in d for t in word):
+            return 0
+        else:
+            return min(d['b']//1, d['a']//1, d['l']//2, d['o']//2, d['n']//1)
