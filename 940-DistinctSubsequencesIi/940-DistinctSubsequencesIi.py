5a67edcc5c2086a40bd8d304d07fc34338ae849d
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:40:25 +0530

    Create 940-DistinctSubsequencesIi

---
+++ 940-DistinctSubsequencesIi/940-DistinctSubsequencesIi.py
@@ -0,0 +1,18 @@
+# Last updated: 9/11/2026, 9:40:23 AM
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 1000000007
+        
+        dp = 1
+        last = [0] * 26
+        
+        for ch in s:
+            index = ord(ch) - ord('a')
+            
+            old_dp = dp
+            
+            dp = (2 * dp - last[index] + MOD) % MOD
+            
+            last[index] = old_dp
+        
+        return (dp - 1 + MOD) % MOD
