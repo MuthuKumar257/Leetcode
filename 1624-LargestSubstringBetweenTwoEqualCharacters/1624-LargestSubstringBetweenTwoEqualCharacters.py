8c23460c7fa16a8907b0d044704218d1f733f104
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:33:04 +0530

    Create 1624-LargestSubstringBetweenTwoEqualCharacters

---
+++ 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.py
@@ -0,0 +1,10 @@
+# Last updated: 9/11/2026, 9:33:02 AM
+class Solution(object):
+    def maxLengthBetweenEqualCharacters(self, s):
+        current = len(s) - 1
+        for i in range(len(s) - 1, 0, -1):
+            for j in range(len(s) - i):
+                if s[j] == s[j + i]:
+                    return i - 1
+        return -1
+        
