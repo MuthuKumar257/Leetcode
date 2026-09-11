b4f1f1ce10d94db38f0ab4e4610e0575c49ce3b4
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:46:15 +0530

    Add Middle of the Linked List solution (Python)

---
+++ leetcode/Easy/Middle-of-the-Linked-List/solution.py
@@ -0,0 +1,17 @@
+"""
+LeetCode: Middle of the Linked List
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/middle-of-the-linked-list/
+"""
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_pointer = head
+        fast_pointer = head
+
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
