759fb4fc5c415689ade4ac2087c80cffb359db7a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:46:20 +0530

    Add Stone Game solution (Python)

---
+++ leetcode/Medium/Stone-Game/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Stone Game
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/stone-game/
+"""
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
