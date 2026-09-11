100e85f1808d20dad17034360636978a18ecf5a9
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:37 +0530

    Add Binary Number with Alternating Bits solution (Java)

---
+++ leetcode/Easy/Binary-Number-with-Alternating-Bits/solution.java
@@ -0,0 +1,13 @@
+/*
+ * LeetCode: Binary Number with Alternating Bits
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/binary-number-with-alternating-bits/
+ */
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+}
