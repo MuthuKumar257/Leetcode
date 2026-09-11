503c5e6330b2a7ee341c856ec2e46b21665955d0
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:38:09 +0530

    Add Word Pattern solution (Python)

---
+++ leetcode/Easy/Word-Pattern/solution.py
@@ -0,0 +1,15 @@
+"""
+LeetCode: Word Pattern
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/word-pattern/
+"""
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        s = s.split()
+
+        return (len(set(pattern)) ==
+                len(set(s)) ==
+                len(set(zip_longest(pattern,s))))
