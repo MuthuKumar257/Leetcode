54b1811141d33d9a19f7a5e7908cb7c5682d1bc8
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 22:07:53 +0530

    Add Valid K-Unique Subarrays I solution (Python)

---
+++ leetcode/Hard/Valid-K-Unique-Subarrays-I/solution.py
@@ -0,0 +1,63 @@
+"""
+LeetCode: Valid K-Unique Subarrays I
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/valid-k-unique-subarrays-i/
+"""
+
+class Solution:
+    def validSubarrays(self, nums: list[int], k: int, queries: list[list[int]]) -> list[bool]:
+        n=len(nums)
+        q=len(queries)
+        mp={}
+        arr=[]
+        for x in nums:
+            if x not in mp:
+                mp[x]=len(mp)
+            
+            arr.append(mp[x])
+            block=int(n**0.5)
+        orders=list(range(q))
+        orders.sort( key=lambda i:(queries[i][0]//block,queries[i][1])  )
+        freq=[0]*len(mp)
+        left=0
+        right=-1
+        distinct=0
+        odd=0
+        ans=[False]*q
+        def add(x):
+                nonlocal distinct,odd
+                if freq[x]==0:
+                    distinct+=1
+                if freq[x]%2==1:
+                    odd-=1
+                freq[x]+=1
+                if freq[x]%2==1:
+                    odd+=1
+                    
+        def remove(x):
+                nonlocal distinct,odd
+                if freq[x]%2==1:
+                    odd-=1
+                freq[x]-=1
+                if freq[x]==0:
+                    distinct-=1
+                if freq[x]%2==1:
+                    odd+=1
+        for i in orders:
+                l,r=queries[i]
+                while right<r:
+                    right+=1
+                    add(arr[right])
+                while right>r:
+                    remove(arr[right])
+                    right-=1
+                while left<l:
+                    remove(arr[left])
+                    left+=1
+                    
+                while left>l:
+                    left-=1
+                    add(arr[left])
+                ans[i]=(distinct==k and odd==0)
+        return ans
