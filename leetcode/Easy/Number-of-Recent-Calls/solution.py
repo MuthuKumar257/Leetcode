b30f8d76d4cd035542db4fd4776257aa23e5c338
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:47:01 +0530

    Add Number of Recent Calls solution (Python)

---
+++ leetcode/Easy/Number-of-Recent-Calls/solution.py
@@ -0,0 +1,24 @@
+"""
+LeetCode: Number of Recent Calls
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/number-of-recent-calls/
+"""
+
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+        
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        
+        while t - self.q[0] > 3000:
+            self.q.popleft()
+            
+        return len(self.q)
+        
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
