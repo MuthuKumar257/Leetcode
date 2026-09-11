fb5d43dc46476dbdddd98b89d01ae35741b9e67a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:28:36 +0530

    Add Spiral Matrix II solution (Python)

---
+++ leetcode/Medium/Spiral-Matrix-II/solution.py
@@ -0,0 +1,18 @@
+"""
+LeetCode: Spiral Matrix II
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/spiral-matrix-ii/
+"""
+
+class Solution:
+    def generateMatrix(self, n):
+        A = [[0] * n for _ in range(n)]
+        i, j, di, dj = 0, 0, 0, 1
+        for k in range(n*n):
+            A[i][j] = k + 1
+            if A[(i+di)%n][(j+dj)%n]:
+                di, dj = dj, -di
+            i += di
+            j += dj
+        return A
