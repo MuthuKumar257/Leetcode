ab547db1ffb55b6dfb15a6868cc96d74a907a96d
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:45:40 +0530

    Add Min Cost Climbing Stairs solution (Python)

---
+++ leetcode/Easy/Min-Cost-Climbing-Stairs/solution.py
@@ -0,0 +1,16 @@
+"""
+LeetCode: Min Cost Climbing Stairs
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/min-cost-climbing-stairs/
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # [10,15,20]
+        n = len(cost)
+        dp = [0]*(n+1)
+        dp[n-1] = cost[n-1]
+        for i in range(n-2,-1,-1):
+            dp[i] = cost[i] + min(dp[i+1],dp[i+2])
+        return min(dp[0],dp[1])
