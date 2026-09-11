d503585fc3bbffe67d756d64e75af3ddee8e3c23
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:26:31 +0530

    Add Reverse Nodes in k-Group solution (Python)

---
+++ leetcode/Hard/Reverse-Nodes-in-k-Group/solution.py
@@ -0,0 +1,39 @@
+"""
+LeetCode: Reverse Nodes in k-Group
+Difficulty: Hard
+Language: Python
+Problem: https://leetcode.com/problems/reverse-nodes-in-k-group/
+"""
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if not head or k == 1:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        curr = head
+
+        # Count the number of nodes in the list
+        count = 0
+        while curr:
+            count += 1
+            curr = curr.next
+
+        # Reverse k nodes at a time
+        while count >= k:
+            curr = prev.next
+            nxt = curr.next
+
+            # Reverse k nodes
+            for _ in range(1, k):
+                curr.next = nxt.next
+                nxt.next = prev.next
+                prev.next = nxt
+                nxt = curr.next
+
+            prev = curr
+            count -= k
+
+        return dummy.next
