28d79db023086cb34dbda755a61e13a26cb1a95b
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:41:18 +0530

    Add Next Greater Element II solution (Java)

---
+++ leetcode/Medium/Next-Greater-Element-II/solution.java
@@ -0,0 +1,20 @@
+/*
+ * LeetCode: Next Greater Element II
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/next-greater-element-ii/
+ */
+
+class Solution {
+        public int[] nextGreaterElements(int[] A) {
+        int n = A.length, res[] = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i % n])
+                res[stack.pop()] = A[i % n];
+            stack.push(i % n);
+        }
+        return res;
+    }
+}
