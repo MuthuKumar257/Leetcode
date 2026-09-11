a75349a11cc5bef85e975cc3dac7c0c5c8ae3027
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:40:43 +0530

    Add 132 Pattern solution (Java)

---
+++ leetcode/Medium/132-Pattern/solution.java
@@ -0,0 +1,29 @@
+/*
+ * LeetCode: 132 Pattern
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/132-pattern/
+ */
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<int[]> st = new Stack<>();
+        int cur_min = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int n = nums[i];
+            while (!st.isEmpty() && n >= st.peek()[0]) {
+                st.pop();
+        }
+
+            if (!st.isEmpty() && n > st.peek()[1]) {
+                return true;
+            }
+
+            st.push(new int[] { n, cur_min });
+            cur_min = Math.min(cur_min, n);
+        }
+
+        return false;        
+    }
+}
