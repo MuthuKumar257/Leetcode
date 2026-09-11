7a098fe9ceb77683491fbf59deb68a576788a1f3
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:40:39 +0530

    Add Assign Cookies solution (Java)

---
+++ leetcode/Easy/Assign-Cookies/solution.java
@@ -0,0 +1,29 @@
+/*
+ * LeetCode: Assign Cookies
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/assign-cookies/
+ */
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0, count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
+                j++;
+            } else if (g[i] > s[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
