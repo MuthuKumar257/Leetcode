eb97b19461524c914e96064983ed988284c34cfe
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:31:06 +0530

    Create 2029-StoneGameIx

---
+++ 2029-StoneGameIx/2029-StoneGameIx.py
@@ -0,0 +1,12 @@
+# Last updated: 9/11/2026, 9:31:02 AM
+class Solution:
+
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt = [0] * 3
+        for stone in stones:
+            cnt[stone % 3] += 1
+
+        if cnt[0] % 2 == 0:
+            return cnt[1] >= 1 and cnt[2] >= 1
+        else:
+            return abs(cnt[1] - cnt[2]) > 2
