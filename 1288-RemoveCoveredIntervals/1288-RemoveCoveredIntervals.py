658a58d4c8bb12687e51da94c2318ad23d173fda
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:38:47 +0530

    Create 1288-RemoveCoveredIntervals

---
+++ 1288-RemoveCoveredIntervals/1288-RemoveCoveredIntervals.py
@@ -0,0 +1,11 @@
+# Last updated: 9/11/2026, 9:38:45 AM
+class Solution:
+    def removeCoveredIntervals(self, A: List[List[int]]) -> int:
+        A.sort(key=lambda x: (x[0], -x[1]))
+        res = r = 0
+
+        for st, end in A:
+            res += end > r
+            r = max(r, end)
+
+        return res
