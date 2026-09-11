dc8eec83304a74b1d386b50e823fe2f417b41ad0
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:45:45 +0530

    Add Jewels and Stones solution (Python)

---
+++ leetcode/Easy/Jewels-and-Stones/solution.py
@@ -0,0 +1,14 @@
+"""
+LeetCode: Jewels and Stones
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/jewels-and-stones/
+"""
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        c=0
+        for i in jewels:
+            if i in stones:
+                c+=stones.count(i)
+        return c
