f72b50604ada65cb33b48f806fb7d62a4333999a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:35:07 +0530

    Add Reverse Bits solution (Java)

---
+++ leetcode/Easy/Reverse-Bits/solution.java
@@ -0,0 +1,18 @@
+/*
+ * LeetCode: Reverse Bits
+ * Difficulty: Easy
+ * Language: Java
+ * Problem: https://leetcode.com/problems/reverse-bits/
+ */
+
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
