a5a1573d6f68bcec659af2af0089ce2101ea1125
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:28:04 +0530

    Add Pow(x, n) solution (Python)

---
+++ leetcode/Medium/Pow(x,-n)/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Pow(x, n)
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/powx-n/
+"""
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
