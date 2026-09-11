607c09ba344381d75144539eeba4df0b8325930f
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:01 +0530

    Add Average of Levels in Binary Tree solution (Python)

---
+++ leetcode/Easy/Average-of-Levels-in-Binary-Tree/solution.py
@@ -0,0 +1,33 @@
+"""
+LeetCode: Average of Levels in Binary Tree
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        q = deque()
+        q.append(root)
+        res = []
+        while q:
+            total, count = 0, 0
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                if node:
+                    total += node.val
+                    count += 1
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            res.append(total / count)
+        return res
