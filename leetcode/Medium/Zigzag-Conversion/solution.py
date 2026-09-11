0f6384cfaeae79823082ae8df1d329ac837cfd05
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:24:53 +0530

    Add Zigzag Conversion solution (Python)

---
+++ leetcode/Medium/Zigzag-Conversion/solution.py
@@ -0,0 +1,17 @@
+"""
+LeetCode: Zigzag Conversion
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/zigzag-conversion/
+"""
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+        a=""
+        for i in range(numRows):
+            for j in range(i,len(s),2*(numRows-1)):
+                a+=s[j]
+                if(i>0 and i<numRows-1 and j+2*(numRows-1)-2*i < len(s)):
+                    a+=s[j+2*(numRows-1)-2*i]
+        return a
