eed524c8447eed40d27a5ef08caf47c69b9a83b5
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:44:20 +0530

    Add Robot Return to Origin solution (Python)

---
+++ leetcode/Easy/Robot-Return-to-Origin/solution.py
@@ -0,0 +1,17 @@
+"""
+LeetCode: Robot Return to Origin
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/robot-return-to-origin/
+"""
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+        position  = 0
+        d = {'R': 1, "L": -1, 'U': 1j, 'D': -1j}
+
+        for ch in moves:
+            position+= d[ch]
+
+        return position == 0
