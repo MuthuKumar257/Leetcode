f03e4289756dc4ef0f812dd054248e1f30bab375
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:29:26 +0530

    Add Sort Colors solution (Python)

---
+++ leetcode/Medium/Sort-Colors/solution.py
@@ -0,0 +1,10 @@
+"""
+LeetCode: Sort Colors
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/sort-colors/
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        return nums.sort()
