964decebf84ec7c3a01937a2fcce88dffc6ff02b
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 09:41:39 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.py
@@ -0,0 +1,22 @@
+# Last updated: 9/11/2026, 9:41:37 AM
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
