bac087e068e9d26b72c424be173c2109141f4c98
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:25:38 +0530

    Add Longest Common Prefix solution (Python)

---
+++ leetcode/Easy/Longest-Common-Prefix/solution.py
@@ -0,0 +1,18 @@
+"""
+LeetCode: Longest Common Prefix
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/longest-common-prefix/
+"""
+
+class Solution:
+    def longestCommonPrefix(self, v: List[str]) -> str:
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans
