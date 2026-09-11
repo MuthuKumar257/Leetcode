b5687358c783bfa8dc29a06b4aa921b9466a108a
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:30:58 +0530

    Add Symmetric Tree solution (Python)

---
+++ leetcode/Easy/Symmetric-Tree/solution.py
@@ -0,0 +1,23 @@
+"""
+LeetCode: Symmetric Tree
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/symmetric-tree/
+"""
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def same(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            return same(root1.left, root2.right) and \
+                   same(root1.right, root2.left)
+        return same(root, root)
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# by ar-sayeem [June 27, 2026]
