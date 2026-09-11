6b41b76f850f2d8a3b98e3e97488feeef5368d01
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-11 21:50:27 +0530

    Add Average Selling Price solution (MySQL)

---
+++ leetcode/Easy/Average-Selling-Price/solution.sql
@@ -0,0 +1,12 @@
+-- LeetCode: Average Selling Price
+-- Difficulty: Easy
+-- Language: MySQL
+-- Problem: https://leetcode.com/problems/average-selling-price/
+
+# Write your MySQL query statement below
+select p.product_id, ifnull( round(SUM(p.price*s.units)/SUM(s.units) ,2 ),0) as average_price
+from Prices p
+left join UnitsSold s 
+on p.product_id=s.product_id and s.purchase_date between p.start_date and p.end_date
+group by product_id
+order by product_id;
