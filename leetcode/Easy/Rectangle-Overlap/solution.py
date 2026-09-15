a7f0e6ade6cc02b8a838c3ea8ecd047144da951b
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-15 10:10:07 +0530

    Add Rectangle Overlap solution (Python)

---
+++ leetcode/Easy/Rectangle-Overlap/solution.py
@@ -0,0 +1,20 @@
+"""
+LeetCode: Rectangle Overlap
+Difficulty: Easy
+Language: Python
+Problem: https://leetcode.com/problems/rectangle-overlap/
+"""
+
+if rec2[0] >= rec1[2]:
+            return False
+
+        if rec2[1] >= rec1[3]:
+            return False
+
+        if rec2[2] <= rec1[0]:
+            return False
+
+        if rec2[3] <= rec1[1]:
+            return False
+
+        return True
