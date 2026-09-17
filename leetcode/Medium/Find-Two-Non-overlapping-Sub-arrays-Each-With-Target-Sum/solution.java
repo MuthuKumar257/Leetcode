612a130bf4aa364bce4f4e56afd6e6703f089817
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-17 09:07:09 +0530

    Add Find Two Non-overlapping Sub-arrays Each With Target Sum solution (Java)

---
+++ leetcode/Medium/Find-Two-Non-overlapping-Sub-arrays-Each-With-Target-Sum/solution.java
@@ -0,0 +1,22 @@
+/*
+ * LeetCode: Find Two Non-overlapping Sub-arrays Each With Target Sum
+ * Difficulty: Medium
+ * Language: Java
+ * Problem: https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/
+ */
+
+}
+
+            sum += arr[r];
+
+            while (sum > target) {
+                sum -= arr[l++];
+        int best = Integer.MAX_VALUE;
+
+        for (int r = 0; r < n; r++) {
+        int n = arr.length;
+        int[] minLen = new int[n];
+        int l = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
