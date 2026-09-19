86db4dcdfa59ed6d0ed8c63c4952e31340c635cb
Muthu Kumar M <muthukumar82508@gmail.com>
2026-09-19 21:01:56 +0530

    Add Circle and Rectangle Overlapping solution (Python)

---
+++ leetcode/Medium/Circle-and-Rectangle-Overlapping/solution.py
@@ -0,0 +1,15 @@
+"""
+LeetCode: Circle and Rectangle Overlapping
+Difficulty: Medium
+Language: Python
+Problem: https://leetcode.com/problems/circle-and-rectangle-overlapping/
+"""
+
+class Solution:
+    def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, 
+    y2: int) -> bool:
+        nearestX = max(x1, min(x2, xCenter))
+        nearestY = max(y1, min(y2, yCenter))
+
+        dist = (nearestX - xCenter) ** 2 + (nearestY - yCenter) ** 2
+        return dist <= radius * radius
